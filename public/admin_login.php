71ae81043ee9a3b5c07cad403eddee866a7a2923
Rohit <rohitsraj12@gmail.com>
2021-03-27 17:12:45 +0530

    Create admin_login.php

---
+++ public/admin_login.php
@@ -0,0 +1,82 @@
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+</head>
+<body class="bg-light">
+    <div class="wrap__body">
+        <div class="body__header">
+        <div class="container">
+                <div class="text-right">
+                    <a class="btn btn-primary" href="admin_register.php">register</a>
+                </div>
+            </div>
+        </div>
+        <!-- end body header -->
+
+        <div class="body__container">
+            <main>
+
+                <div class="container">
+                
+                    <div class="row">
+
+                        <div class="col-md-5">
+
+                            <div class="section__form">
+                                <header class="form__header">
+                                    <h4 class="h4">
+                                        Welcome back
+                                    </h4>
+
+                                    <h4 class="h2">
+                                        Login to your account
+                                    </h4>
+                                </header>
+
+                                <div class="form__body">
+                                    <form action="" method="">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">Email address</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputPassword1">Password</label>
+                                            <input type="password" class="form-control" id="exampleInputPassword1">
+                                        </div>
+                                        <div class="form-group form-check">
+                                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                       
+                                    </form>
+                                </div>
+                                <!-- wrap form -->
+
+                            </div>
+
+                        </div>
+
+                        <div class="col-md-7">
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </main>
+        </div>
+    </div>
+</body>
+</html>
