56fa1c097a1f2a545a9e462fd332633d995b0b48
Rohit <rohitsraj12@gmail.com>
2021-03-27 17:12:41 +0530

    Create admin_header.php

---
+++ private/required/admin/admin_header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $page_title; ?></title>
+</head>
+<body>
+<div class="body__wrap">
+    <div class="body__header">
+        <div class="header__brand">
+            <a href="">logo</a>
+        </div>
+
+        <nav class="header__nav">
+            <ul>
+                <li class="nav__list"><a href="" class="nav__link">home</a></li>
+                <li class="nav__list"><a href="" class="nav__link">insert teacher record</a></li>
+                <li class="nav__list"><a href="" class="nav__link">profile</a></li>
+                <li class="nav__list"><a href="" class="nav__link"></a></li>
+                <li class="nav__list"><a href="" class="nav__link">log out</a></li>
+            </ul>
+        </nav>
+    </div>
+
+</div>
