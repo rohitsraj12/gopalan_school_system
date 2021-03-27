f84929d022f19bda2037ae057f3c0f39b6012896
Rohit <rohitsraj12@gmail.com>
2021-03-27 17:12:35 +0530

    Create db_connect.php

---
+++ private/config/db_connection/db_connect.php
@@ -0,0 +1,12 @@
+<?php
+
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "gopalan_school";
+
+    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+    if(!$conn){
+        die("database connection failed");
+    }
