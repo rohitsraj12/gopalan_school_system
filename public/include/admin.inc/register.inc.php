552325fd4fab3c18708af4b1ad29b9787d99be98
Rohit <rohitsraj12@gmail.com>
2021-03-29 17:13:25 +0530

    Update register.inc.php

--- public/include/admin.inc/register.inc.php
+++ public/include/admin.inc/register.inc.php
@@ -72,7 +72,7 @@ if(isset($_POST['submit-register'])){
                 //stop scripting
                 exit();
             } else {
-                $first_name = "";
+                // $first_name = "";
                 $sql = "INSERT INTO admins (first_name, last_name, emp_id, emp_email, admin_password, emp_status) 
                 VALUES (?, ?, ?, ?, ?, ?)";
                 $stmt = mysqli_stmt_init($conn);
@@ -88,22 +88,8 @@ if(isset($_POST['submit-register'])){
 
                     mysqli_stmt_bind_param($stmt, "sssssi", $first_name, $last_name, $emp_id, $email, $hashed_password, $status);
                     mysqli_stmt_execute($stmt);
-                    
-                    // $to = $email;
-                    // $subject = "Hi " . $emp_id . ", Welcome to facultyforyou.com";
-                    // $message = "<p>Dear " . $emp_id . ",</p></br>";
-                    // $message .= "<p>Welcome to <a href='http://facultyforyou.com/'>facultyforyou.com.</p></br>";
-                    // $message .= "<p>A special thanks to you as you now became a new member on most honored and credible learning network in India. Hope we will provide you a best tutor on your requirement soon.</p></br>";
-                    // $message .= "<p>Thank you.,</p>";
-                    // $message .= "<p>Facultyforyou.com</p>";
-                    // $message .= "<div><img width='250px' src='http://facultyforyou.com/img/brand/faculty_for_you_brand.png'></div>";
-                    
-                    // $headers = "From: facultyforyou.com <nathanisrinivasvictory@gmail.com>\r\n";
-                    // $headers .= "Replay-To: rohitsraj12@gmail.com\r\n";
-                    // $headers .= "Content-type: text/html\r\n";
                 
-                    // mail($to, $subject, $message, $headers);
-                
+                    // if email need to send below add email script 
 
                     header("Location: ../../admin_login.php?register=success");
                     exit();
