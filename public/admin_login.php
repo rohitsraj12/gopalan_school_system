086ab912bf3efb284906e1beecfc2b14e61f1bbe
Rohit <rohitsraj12@gmail.com>
2021-03-29 12:53:38 +0530

    Update admin_login.php

--- public/admin_login.php
+++ public/admin_login.php
@@ -1,7 +1,4 @@
 
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -43,21 +40,21 @@
                                 </header>
 
                                 <div class="form__body">
-                                    <form action="" method="">
+                                    <form action="include\admin.inc\login.inc.php" method="POST">
                                         <div class="form-group">
-                                            <label for="exampleInputEmail1">Email address</label>
-                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                                            <label for="email">Email address</label>
+                                            <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
+                                            <!-- <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small> -->
                                         </div>
                                         <div class="form-group">
-                                            <label for="exampleInputPassword1">Password</label>
-                                            <input type="password" class="form-control" id="exampleInputPassword1">
+                                            <label for="password">Password</label>
+                                            <input type="password" name="password" class="form-control" id="password">
                                         </div>
-                                        <div class="form-group form-check">
+                                        <!-- <div class="form-group form-check">
                                             <input type="checkbox" class="form-check-input" id="exampleCheck1">
                                             <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Submit</button>
+                                        </div> -->
+                                        <button type="submit" name="login-admin" class="btn btn-primary">Submit</button>
                                        
                                     </form>
                                 </div>
@@ -80,3 +77,4 @@
     </div>
 </body>
 </html>
+
