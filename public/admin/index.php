c6a9058d44ef33ca4035782cd3b6b8a412cd07f8
Rohit <rohitsraj12@gmail.com>
2021-03-29 17:13:20 +0530

    Update index.php

--- public/admin/index.php
+++ public/admin/index.php
@@ -1,21 +1,42 @@
 <?php
-    $page_title = "admin dashboard";
 
-    $db_host = "localhost";
-    $db_user = "root";
-    $db_pass = "";
-    $db_name = "gopalan_school";
 
-    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    session_start();
 
-    if(!$conn){
-        die("database connection failed");
+    if(!isset($_SESSION["emp_id"])){
+        header('location: ../admin_login.php');
     }
 
-    $query = "SELECT * FROM admin";
+    $page_title = "admin dashboard";
+
+    // $db_host = "localhost";
+    // $db_user = "root";
+    // $db_pass = "";
+    // $db_name = "gopalan_school";
+
+    // $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+    // if(!$conn){
+    //     die("database connection failed");
+    // }
+    
+    
+    include('../../private/config/db_connection/db_connect.php');
+    // include('../../private/config/config.php');
+
+
+
+    $id = $_SESSION['emp_id'];
+
+    $query = "SELECT * FROM admins WHERE emp_id='$id'";
     $result = mysqli_query($conn, $query);
 
     $row = mysqli_fetch_assoc($result);
+    echo $row['first_name'];
+
+
+
+
 
     include('../../private/required/admin/admin_header.php');
 
@@ -23,7 +44,7 @@
 
 
     <h1>
-       <?php echo $row['admin_email']?>;
+       <?php echo $row['first_name'];?>
     </h1>
 
 
