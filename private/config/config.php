2cc76e7b4566d0016c7f41c3a94f38bb052c3143
Rohit <rohitsraj12@gmail.com>
2021-03-31 17:18:58 +0530

    Update config.php

--- private/config/config.php
+++ private/config/config.php
@@ -1,5 +1,5 @@
 <?php
 
-    function base_url($extention){
-        echo "http://localhost/gopalan_system/gopalan_school_system/public/" . $extention; 
+    function base_url(){
+        echo "http://localhost/gopalan_system/gopalan_school_system/public/"; 
     }
