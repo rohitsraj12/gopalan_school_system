ef466b2b1b9a421f35cdc0e886fda7e2f6c16a79
Rohit <rohitsraj12@gmail.com>
2021-03-31 17:19:08 +0530

    Update admin_header.php

--- private/required/admin/admin_header.php
+++ private/required/admin/admin_header.php
@@ -1,33 +1,201 @@
-<?php
-    // function base_url(){
-    //     echo "http://localhost/gopalan_system/gopalan_school_system/public/"; 
-    // }
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<meta charset="utf-8">
+	<meta name="description" content="Miminium Admin Template v.1">
+	<meta name="author" content="Isna Nur Azis">
+	<meta name="keyword" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $page_title; ?></title>
-</head>
-<body>
-<div class="body__wrap">
-    <div class="body__header">
-        <div class="header__brand">
-            <a href="">logo</a>
-        </div>
+ 
+    <!-- start: Css -->
+    <link rel="stylesheet" type="text/css" href="<?php base_url()?>css/bootstrap.min.css">
 
-        <nav class="header__nav">
-            <ul>
-                <li class="nav__list"><a href="" class="nav__link">home</a></li>
-                <li class="nav__list"><a href="" class="nav__link">insert teacher record</a></li>
-                <li class="nav__list"><a href="" class="nav__link">profile</a></li>
-                <li class="nav__list"><a href="" class="nav__link"></a></li>
-                <li class="nav__list"><a href="http://localhost/gopalan_system/gopalan_school_system/public/logout.php" class="nav__link">log out</a></li>
-            </ul>
+      <!-- plugins -->
+      <link rel="stylesheet" type="text/css" href="<?php base_url()?>css/plugins/font-awesome.min.css"/>
+      <link rel="stylesheet" type="text/css" href="<?php base_url()?>css/plugins/simple-line-icons.css"/>
+      <link rel="stylesheet" type="text/css" href="<?php base_url()?>css/plugins/animate.min.css"/>
+      <link rel="stylesheet" type="text/css" href="<?php base_url()?>css/plugins/fullcalendar.min.css"/>
+      <!-- table plugin -->    
+        <link rel="stylesheet" type="text/css" href="<?php base_url()?>css/plugins/datatables.bootstrap.min.css"/>
+
+	<link href="<?php base_url()?>css/style.css" rel="stylesheet">
+	<!-- end: Css -->
+
+	<link rel="shortcut icon" href="<?php base_url()?>img/logomi.png">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+   
+ <body id="mimin" class="dashboard">
+
+
+      <!-- start: Header -->
+        <nav class="navbar navbar-default header navbar-fixed-top">
+          <div class="col-md-12 nav-wrapper">
+            <div class="navbar-header" style="width:100%;">
+              <div class="opener-left-menu is-open">
+                <span class="top"></span>
+                <span class="middle"></span>
+                <span class="bottom"></span>
+              </div>
+                <a href="index.html" class="navbar-brand"> 
+                 <img src="<?php base_url();?>img/gis-logo.svg" alt="user name" />
+                </a>
+
+              <ul class="nav navbar-nav search-nav">
+                <li>
+                   <div class="search">
+                    <span class="fa fa-search icon-search" style="font-size:23px;"></span>
+                    <div class="form-group form-animate-text">
+                      <input type="text" class="form-text" required>
+                      <span class="bar"></span>
+                      <label class="label-search">Type anywhere to <b>Search</b> </label>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+
+              <ul class="nav navbar-nav navbar-right user-nav">
+                <li class="user-name"><span><?php echo $row['first_name'];?></span></li>
+                  <li class="dropdown avatar-dropdown">
+                   <img src="<?php base_url();?>img/avatar.jpg" class="img-circle avatar" alt="user name" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"/>
+                   <ul class="dropdown-menu user-dropdown">
+                     <li><a href="<?php base_url();?>"><span class="fa fa-user"></span> My Profile</a></li>
+                     <li><a href="<?php base_url();?>"><span class="fa fa-calendar"></span> My Calendar</a></li>
+                     <li><a href="<?php base_url();?>logout.php">logout</a></li>
+                     <li role="separator" class="divider"></li>
+                     <li class="more">
+                      <ul>
+                        <li><a href=""><span class="fa fa-cogs"></span></a></li>
+                        <li><a href=""><span class="fa fa-lock"></span></a></li>
+                        <li><a href=""><span class="fa fa-power-off "></span></a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </li>
+                <li ><a href="#" class="opener-right-menu"><span class="fa fa-coffee"></span></a></li>
+              </ul>
+            </div>
+          </div>
         </nav>
-    </div>
+      <!-- end: Header -->
 
-</div>
+      <!-- start:Left Menu -->
+      <div id="left-menu">
+              <div class="sub-left-menu scroll">
+                <ul class="nav nav-list">
+                    <li><div class="left-bg"></div></li>
+                    <li class="time">
+                      <h1 class="animated fadeInLeft">21:00</h1>
+                      <p class="animated fadeInRight">Sat,October 1st 2029</p>
+                    </li>
+                    <li class="active ripple">
+                      <a class="tree-toggle nav-header"><span class="fa-home fa"></span> Dashboard 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                      <ul class="nav nav-list tree">
+                          <li><a href="<?php base_url()?>admin/">index</a></li>
+                      </ul>
+                    </li>
+                    <li class="ripple">
+                      <a class="tree-toggle nav-header">
+                        <span class="fa-diamond fa"></span> Teachers
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                      <ul class="nav nav-list tree">
+                        <li><a href="<?php base_url()?>admin/teacher/">View Teacher list</a></li>
+                        <li><a href="boxed.html">Create Teacher Profile</a></li>
+                        <!-- <li><a href="boxed.html">Boxed</a></li> -->
+                      </ul>
+                    </li>
+                    <li class="ripple">
+                      <a class="tree-toggle nav-header">
+                        <span class="fa-area-chart fa"></span>Blogs
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                      <ul class="nav nav-list tree">
+                        <li><a href="morris.html">View</a></li>
+                        <li><a href="chartjs.html">Create</a></li>
+                      </ul>
+                    </li>
+                    <!-- <li class="ripple"><a class="tree-toggle nav-header">
+                        <span class="fa fa-pencil-square"></span> Ui Elements  <span class="fa-angle-right fa right-arrow text-right"></span> </a>
+                      <ul class="nav nav-list tree">
+                        <li><a href="color.html">Color</a></li>
+                        <li><a href="weather.html">Weather</a></li>
+                        <li><a href="typography.html">Typography</a></li>
+                        <li><a href="icons.html">Icons</a></li>
+                        <li><a href="buttons.html">Buttons</a></li>
+                        <li><a href="media.html">Media</a></li>
+                        <li><a href="panels.html">Panels & Tabs</a></li>
+                        <li><a href="notifications.html">Notifications & Tooltip</a></li>
+                        <li><a href="badges.html">Badges & Label</a></li>
+                        <li><a href="progress.html">Progress</a></li>
+                        <li><a href="sliders.html">Sliders</a></li>
+                        <li><a href="timeline.html">Timeline</a></li>
+                        <li><a href="modal.html">Modals</a></li>
+                      </ul>
+                    </li>
+                    <li class="ripple"><a class="tree-toggle nav-header"><span class="fa fa-check-square-o"></span> Forms  <span class="fa-angle-right fa right-arrow text-right"></span> </a>
+                      <ul class="nav nav-list tree">
+                        <li><a href="formelement.html">Form Element</a></li>
+                        <li><a href="#">Wizard</a></li>
+                        <li><a href="#">File Upload</a></li>
+                        <li><a href="#">Text Editor</a></li>
+                      </ul>
+                    </li>
+                    <li class="ripple"><a class="tree-toggle nav-header"><span class="fa fa-table"></span> Tables  <span class="fa-angle-right fa right-arrow text-right"></span> </a>
+                      <ul class="nav nav-list tree">
+                        <li><a href="datatables.html">Data Tables</a></li>
+                        <li><a href="handsontable.html">handsontable</a></li>
+                        <li><a href="tablestatic.html">Static</a></li>
+                      </ul>
+                    </li>
+                    <li class="ripple"><a href="calendar.html"><span class="fa fa-calendar-o"></span>Calendar</a></li>
+                    <li class="ripple"><a class="tree-toggle nav-header"><span class="fa fa-envelope-o"></span> Mail <span class="fa-angle-right fa right-arrow text-right"></span> </a>
+                      <ul class="nav nav-list tree">
+                        <li><a href="mail-box.html">Inbox</a></li>
+                        <li><a href="compose-mail.html">Compose Mail</a></li>
+                        <li><a href="view-mail.html">View Mail</a></li>
+                      </ul>
+                    </li>
+                    <li class="ripple"><a class="tree-toggle nav-header"><span class="fa fa-file-code-o"></span> Pages  <span class="fa-angle-right fa right-arrow text-right"></span> </a>
+                      <ul class="nav nav-list tree">
+                        <li><a href="forgotpass.html">Forgot Password</a></li>
+                        <li><a href="login.html">SignIn</a></li>
+                        <li><a href="reg.html">SignUp</a></li>
+                        <li><a href="article-v1.html">Article v1</a></li>
+                        <li><a href="search-v1.html">Search Result v1</a></li>
+                        <li><a href="productgrid.html">Product Grid</a></li>
+                        <li><a href="profile-v1.html">Profile v1</a></li>
+                        <li><a href="invoice-v1.html">Invoice v1</a></li>
+                      </ul>
+                    </li>
+                    <li class="ripple"><a class="tree-toggle nav-header"><span class="fa "></span> MultiLevel  <span class="fa-angle-right fa right-arrow text-right"></span> </a>
+                      <ul class="nav nav-list tree">
+                        <li><a href="view-mail.html">Level 1</a></li>
+                        <li><a href="view-mail.html">Level 1</a></li>
+                        <li class="ripple">
+                          <a class="sub-tree-toggle nav-header">
+                            <span class="fa fa-envelope-o"></span> Level 1
+                            <span class="fa-angle-right fa right-arrow text-right"></span>
+                          </a>
+                          <ul class="nav nav-list sub-tree">
+                            <li><a href="mail-box.html">Level 2</a></li>
+                            <li><a href="compose-mail.html">Level 2</a></li>
+                            <li><a href="view-mail.html">Level 2</a></li>
+                          </ul>
+                        </li>
+                      </ul>
+                    </li> -->
+                    <li><a href="credits.html">Credits</a></li>
+                  </ul>
+                </div>
+            </div>
+          <!-- end: Left Menu -->
