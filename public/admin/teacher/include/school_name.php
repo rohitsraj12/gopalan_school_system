e9453b964b15ca2169a7372ca96282cec02f9b49
Rohit <rohitsraj12@gmail.com>
2021-04-02 17:12:08 +0530

    Create school_name.php

---
+++ public/admin/teacher/include/school_name.php
@@ -0,0 +1,13 @@
+<?php
+
+    // function school_all(table){
+    //     // $table =  table;
+    //     // $query = "SELECT * FROM $table;
+    //     // $return = mysqli_query($conn, $query);
+
+    //     // return $table;
+    //     // return $return;
+    // }
+
+    echo "hi";
+?>
