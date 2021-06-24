55b5c38c5fc14ee7dffe979e4bab420c5f4c74c8
Rohit <rohitsraj12@gmail.com>
2021-06-24 17:09:52 +0530

    Update update_teacher_account.inc.php

--- public/admin/teacher/include/update_teacher_account.inc.php
+++ public/admin/teacher/include/update_teacher_account.inc.php
@@ -1,112 +1,56 @@
 <?php
 
 if(isset($_POST['update_profile'])){
+    require_once '../../../../private/config/config.php';
+    require_once '../../../../private/config/db_connection/db_connect.php';
 
-require_once '../../../../private/config/db_connection/db_connect.php';
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$emp_id = $_POST['emp_id'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$re_password = $_POST['re_password'];
-$school = $_POST['school_name'];
-$position = $_POST['position'];
-$class = $_POST['grade'];
-$section = $_POST['section'];
-$status = '1';
+    $teacher_id = $_POST['teacher_id'];
 
-// echo $first_name . ", " . $last_name . ", " . $emp_id . ", " . $email . ", ". $password . ", " . $re_password . ", " . $status;
+    // echo $teacher_id;
 
-//if empty field condition
-if(empty($first_name) || empty($last_name) || empty($emp_id) || empty($email) || empty($password) || empty($re_password)){
-    // redirect to register and empty field
-    header("location: ../create_new_teacher.php?error=emptyfields&user_name=".$emp_id."&mail".$email);
-    //stop scripting
-    exit();
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $emp_id = $_POST['emp_id'];
+    $email = $_POST['email'];
+    // $password = $_POST['password'];
+    // $re_password = $_POST['re_password'];
+    $school = $_POST['school_name'];
+    $position = $_POST['position'];
+    $class = $_POST['grade'];
+    $section = $_POST['section'];
+    // $status = '1';
 
-} else if (!filter_var($email, FILTER_VALIDATE_EMAIL) && !preg_match("/^[a-zA-Z0-9]*$/", $emp_id)){
-    // redirect to register and empty field
-    header("location: ../create_new_teacher.php?error=invalidmailuser_name");
-    //stop scripting
-    exit();
+    $teacher_query      = "SELECT * FROM teachers WHERE teacher_id = '$teacher_id'";
+    $teacher_result     = mysqli_query($conn, $teacher_query);
+    $row                = mysqli_fetch_assoc($teacher_result); 
 
-} else if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
-    // redirect to register and empty field
-    header("location: ../create_new_teacher.php?error=invalidmail&user_name=".$emp_id);
-    //stop scripting
-    exit();
+    if(empty($_POST['first_name'])){ $first_name = $row['first_name'] ;}
+    if(empty($_POST['last_name'])){ $last_name = $row['last_name'] ;} 
+    if(empty($_POST['emp_id'])){ $emp_id = $row['emp_id'] ;} 
+    if(empty($_POST['email'])){ $email = $row['email'] ;} 
 
-} else if (!preg_match("/^[a-zA-Z0-9]*$/", $emp_id)){
-    // redirect to register and empty field
-    header("location: ../create_new_teacher.php?error=invalidmail&mail=".$email);
-    //stop scripting
-    exit();
+    // echo $first_name . ", " . $last_name . ", " . $emp_id . ", " . $email . ", ". $school . ", " . $position . ", " . $class . ", ". $section . ", " . $status;
 
-} else if ($password !== $re_password){
-    // redirect to register and empty field
-    header("location: ../create_new_teacher.php?error=passwordcheck&user_name=".$emp_id."&mail".$email);
-    //stop scripting
-    exit();
+    $sql = "UPDATE `teachers` SET `first_name`= '$first_name',
+                                `last_name`= '$last_name',
+                                `emp_id`= '$emp_id',
+                                `email`= '$email',
+                                `position_id`= '$position',
+                                `school_id`= '$school',
+                                `class_id`= '$class',
+                                `section_id`= '$section' 
+                                WHERE `teacher_id` = $teacher_id";
+    
+    $result = mysqli_query($conn, $sql);
 
-} else {
-    // prepared statement (? placeholder for safty)
-    $sql = "SELECT emp_id FROM teachers WHERE emp_id=?";
-    // 
-    $stmt = mysqli_stmt_init($conn);
-
-    // 
-    if(!mysqli_stmt_prepare($stmt, $sql)){
-        
-        // redirect to register and empty field
-        header("location: ../create_new_teacher.php?error=sqlerror");
-        //stop scripting
-        exit();
+    if(!$result){
+        echo "update failed";
     } else {
-        // 
-        mysqli_stmt_bind_param($stmt, "s", $emp_id);
-        mysqli_stmt_execute($stmt);
-        // is ther any match
-        mysqli_stmt_store_result($stmt);
-        $resultcheck = mysqli_stmt_num_rows($stmt);
+        // echo "record updated";
 
-        if($resultcheck > 0 ){
-            
-            // redirect to register and empty field
-            header("Location: ../create_new_teacher.php?error=usertaken$mail=".$email);
-            //stop scripting
-            exit();
-        } else {
-            // $first_name = "";
-            $sql = "INSERT INTO teachers (first_name, last_name, emp_id, email, teacher_password, teacher_status, school_id, position_id, class_id, section_id) 
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-            $stmt = mysqli_stmt_init($conn);
-
-            if(!mysqli_stmt_prepare($stmt, $sql)){
-                // redirect to register and empty field
-                header("Location: ../create_new_teacher.php?error=sqlerror");
-                //stop scripting
-                exit();
-            } else {
-                // secure password
-                $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-                mysqli_stmt_bind_param($stmt, "sssssiiiii", $first_name, $last_name, $emp_id, $email, $hashed_password, $status, $school, $position, $class, $section);
-                mysqli_stmt_execute($stmt);
-            
-                // if email need to send below add email script 
-
-                header("Location: ../create_new_teacher.php?register=success");
-                exit();
-            }
-        }
-        }
+        header ("Location: ../view_teacher_profile.php?id=" . $emp_id . "&message=update_seccess");
     }
-    mysqli_stmt_close($stmt);
-    mysqli_close($conn);
 
-} else {
- // redirect to register and empty field
- header("location: ../create_new_teacher.php");
- //stop scripting
- exit();
+
+                
 }
