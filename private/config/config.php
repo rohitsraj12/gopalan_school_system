c5dc40ca0bca67ab5891fe1a50867781d3ff0b1f
Rohit <rohitsraj12@gmail.com>
2021-06-25 16:37:09 +0530

    Update config.php

--- private/config/config.php
+++ private/config/config.php
@@ -2,5 +2,7 @@
 
     function base_url(){
         echo "http://localhost/gopalan_system/gopalan_school_system/public/"; 
+
+        // echo "http://gopalanschool.com/school_system/public/";
         
     }
