d84f9d530d6ee75eb0c35a8f5f01a6ead438d0c9
Rohit <rohitsraj12@gmail.com>
2021-06-25 16:33:55 +0530

    Update db_connect.php

--- private/config/db_connection/db_connect.php
+++ private/config/db_connection/db_connect.php
@@ -1,15 +1,14 @@
 <?php
 
+    // $db_host = "localhost";
+    // $db_user = "uiglsjvztkuay";
+    // $db_pass = "gopalan@schools";
+    // $db_name = "dbudubglbge2if";
 
     $db_host = "localhost";
-    $db_user = "uiglsjvztkuay";
-    $db_pass = "$^g$1849h8sh";
-    $db_name = "dbudubglbge2if";
-
-    // $db_host = "localhost";
-    // $db_user = "rot";
-    // $db_pass = "";
-    // $db_name = "gopalan_school";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "gopalan_school";
 
     $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
