88edd58be091a2ee12796fb68c0c4cf8be66c0b1
Rohit <rohitsraj12@gmail.com>
2021-07-01 17:41:25 +0530

    Update attendance.php

--- public/students/attendance.php
+++ public/students/attendance.php
@@ -144,7 +144,7 @@
                  <div class="panel box-v1">
                     <div class="panel-heading bg-white border-none">
                         <div class="col-md-6 col-sm-6 col-xs-6 text-left padding-0">
-                            <h4 class="text-left">Marksheet</h4>
+                            <h4 class="text-left">Absent Dates</h4>
                         </div>
                         <!-- <div class="col-md-6 col-sm-6 col-xs-6 text-right">
                             <h4>
