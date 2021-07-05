fdb75dc14a1d4587b3affe65151cb7ed00fccf19
Rohit <rohitsraj12@gmail.com>
2021-07-05 17:35:59 +0530

    Update attendance.php

--- public/students/attendance.php
+++ public/students/attendance.php
@@ -1,21 +1,26 @@
 <?php
 
-    // session_start();
+    session_start();
 
-    // if(!isset($_SESSION["emp_id"])){
-    //     header('location: ../../admin_login.php');
-    // }
-    $page_title = "Student Home";
+    if(!isset($_SESSION["student_user_id"])){
+        header('location: ../../admin_login.php');
+    }
+    $page_title = "Attendance Details";
 
     include('../../private/config/db_connection/db_connect.php');
     include('../../private/config/config.php');
 
-    // $id = $_SESSION['student_id'];
-    $id = 2;
+    $id = $_SESSION['id'];
+    // $id = 2;
 
     // echo $id;
 
-    $query = "SELECT * FROM students WHERE student_id='$id'";
+    $query = "SELECT students.*, class_rooms.*, class_sections.* FROM students
+            JOIN class_rooms
+                ON class_rooms.class_id = students.class_id
+            JOIN class_sections
+                ON class_sections.section_id = students.section_id
+            WHERE student_id='$id'";
     $result = mysqli_query($conn, $query);
 
     $row = mysqli_fetch_assoc($result);
@@ -66,64 +71,93 @@
         </div> -->
      </div>
       <div class="col-md-offset-1 col-md-3" style="padding-top: 10px">
-          <div class="panel">
-            <div class="panel-heading bg-amber border-none text-center">
-              <h4>Student details</h4>
-            </div>
-
-            <div class="col-md-12 profile-v1-cover-wrap" style="padding-right:0px;">
-
-                <div class="col-md-12 profile-v1 text-center" style="padding-top: 10px">
-                    <img style="width: 100px" src="<?php if(empty($row['studdent_img'])) { echo '../img/teacher/avatar.jpg';}else { echo '$teacher_row["student_img"]';}?>"/>
-                    <h2><?php echo $full_name; ?></h2>
-                </div>
-            </div>
-
-            <div class="panel-body">
-              <div class="panel-body text-capitalize">
-                <dl class="dl-horizontal">
-                  <dt>Student Name</dt>
-                    <dd><?php echo $row['first_name']. " " . $row['last_name'] ?></dd>
-                  </br>
-
-                  <dt>Student Id</dt>
-                    <dd><?php echo $row['student_user_id']?></dd>
-                  </br>
-
-                  <dt>Email</dt>
-                    <dd  class="text-lowercase"><?php echo $row['email']?></dd>
-                  </br>
-
-                  <dt>Phone Number</dt>
-                    <dd  class="text-lowercase"><?php echo $row['phone']?></dd>
-                  </br>
-
-                  <dt>Class and division</dt>
-                    <dd><?php if($row['class_id'] == 1){ echo "Active";} ?></dd>
-                  </br>
-
-                </dl>
-
-                <div class="text-center">
-
-                    <a href="<?php base_url()?>students/" class="btn btn-danger">Home page</a>
-                
-                </div>
-
-              </div>
-            </div>
-            <!-- <div class="panel-footer bg-white border-none">
-                <center>
-                  <input type="button" value="download as pdf" class="btn btn-danger box-shadow-none"/>
-                </center>
-            </div> -->
-          </div>
+          <?php
+            include_once 'include/aside_profile.php';
+          ?>
         </div>
      </div>
 
      <div class="col-md-7">
          <div class="row">
-             <div class="col-md-6">
+           
+
+         <div class="col-md-12">
+                
+                <div class="panel">
+                      <div class="panel-heading"><h3>Absent Dates</h3></div>
+                      <div class="panel-body">
+                        <div class="responsive-table">
+                        <table id="datatables-example" class="table table-striped table-bordered" width="100%" cellspacing="0">
+                        <thead>
+                          <tr>
+                            <th>Number of Absent</th>
+                            <th>Date of Absent</th>
+                            <th>Note</th>
+                            <!-- <th>Emp Id</th> -->
+                            <th>Update Note</th>
+                            <!-- <th>View Details</th> -->
+                          </tr>
+                        </thead>
+                          <tbody>
+                          <?php 
+                            // $query = "SELECT * FROM teachers";
+
+                            // $query = "SELECT teachers.*, schools.*, teacher_positions.* FROM teachers
+                            //   JOIN schools
+                            //       ON schools.school_id = teachers.school_id
+                            //   JOIN teacher_positions
+                            //       ON teacher_positions.position_id = teachers.position_id";
+
+                              $query = "SELECT teachers.*, schools.*, teacher_positions.*, class_rooms.*, class_sections.* FROM teachers
+                              JOIN schools
+                                  ON schools.school_id = teachers.school_id
+                              JOIN teacher_positions
+                                  ON teacher_positions.position_id = teachers.position_id
+                              JOIN class_rooms
+                                  ON class_rooms.class_id = teachers.class_id
+                              JOIN class_sections
+                                  ON class_sections.section_id = teachers.section_id";
+                              
+                              $result = mysqli_query($conn, $query);
+
+                              // $row_num = mysqli_num_rows($result);
+
+                              while($rows = mysqli_fetch_assoc($result)){
+                            ?>
+                            
+                              <tr>
+                              <td><?php echo  1;?></td>
+
+                                <td><?php echo '10/06/2021';?></td>
+                                <td><?php echo "student can write the reason";
+                                
+                                ?></td>
+                                <td><a href="update_teacher_profile.php?id=<?php echo $rows['emp_id'];?>">update note</a></td>
+                                <!-- <td><a href="view_teacher_profile.php?id=<?php echo $rows['emp_id'];?>">view details</a></td> -->
+                              </tr>
+
+                            <?php
+                              }
+                            ?>
+                          </tbody>
+                          </table>
+                        </div>
+                </div>
+            </div>
+
+            <div class="col-md-12">
+                <div class="panel">
+                       <div class="panel-heading-white panel-heading">
+                          <h4>Monthly Attendance Record</h4>
+                        </div>
+                        <div class="panel-body">
+                            <div class="col-md-12">
+                             <canvas id="bar" class="bar-chart"></canvas>
+                            </div>
+                        </div>
+                  </div>
+            </div>
+             <!-- <div class="col-md-6">
                 <div class="panel">
                     <div class="panel-heading-white panel-heading text-center">
                         <h4>Attendance</h4>
@@ -138,19 +172,14 @@
                         </center>
                     </div>
                 </div>
-             </div>
+             </div> -->
              
-             <div class="col-md-4">
+             <!-- <div class="col-md-4">
                  <div class="panel box-v1">
                     <div class="panel-heading bg-white border-none">
                         <div class="col-md-6 col-sm-6 col-xs-6 text-left padding-0">
                             <h4 class="text-left">Absent Dates</h4>
                         </div>
-                        <!-- <div class="col-md-6 col-sm-6 col-xs-6 text-right">
-                            <h4>
-                            <span class="icon-basket-loaded icons icon text-right"></span>
-                            </h4>
-                        </div> -->
                         </div>
                         <div class="panel-body text-center">
                             <h1>4</h1>
@@ -158,7 +187,7 @@
                         <p>Last Month</p>
                     </div>
                 </div>
-             </div>
+             </div> -->
          </div>
      </div>
    
