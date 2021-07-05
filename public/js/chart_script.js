dc36d908cc76aee26ad989aa203516ef8492d340
Rohit <rohitsraj12@gmail.com>
2021-07-05 17:35:52 +0530

    Update chart_script.js

--- public/js/chart_script.js
+++ public/js/chart_script.js
@@ -1,26 +1,3 @@
-// $(document).ready(function(){
-
-//     var selId = document.getElementsById("attendance").getContext("2d");
-
-//     var peiChart = new Chart(selId, {
-//         type: 'pei',
-//         labels: [
-//             'Red',
-//             'Yellow'
-//           ],
-//           datasets: [{
-//             label: 'My First Dataset',
-//             data: [95, 5],
-//             backgroundColor: [
-//               'rgb(255, 99, 132)',
-//               'rgb(255, 205, 86)'
-//             ],
-//             hoverOffset: 4
-//           }]
-//     })
-
-// })
-
 (function(jQuery){
     // var radarData = {
     //     labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
@@ -132,59 +109,82 @@
     //     ];
 
         
-    var attendance = [
-        {
-            value: 85,
-            color:"#5465ff",
-            highlight: "#9bb1ff",
-            label: "present",
-            hoverOffset: 4
-        },
-        {
-            value: 15,
-            color: "#b9375e",
-            highlight: "#ff7aa2",
-            label: "Absent",
-            hoverOffset: 4
-        }
+    // var attendance = [
+    //     {
+    //         value: 85,
+    //         color:"#5465ff",
+    //         highlight: "#9bb1ff",
+    //         label: "present",
+    //         hoverOffset: 4
+    //     },
+    //     {
+    //         value: 15,
+    //         color: "#b9375e",
+    //         highlight: "#ff7aa2",
+    //         label: "Absent",
+    //         hoverOffset: 4
+    //     }
 
-    ];
+    // ];
 
-         window.onload = function(){
-            // var ctx = $(".doughnut-chart")[0].getContext("2d");
-            // window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {
-            //     responsive : true,
-            //     showTooltips: true
-            // });
+    
+    var bar = {
+        labels: ["June", "July", "August", "September", "October", "November", "December"],
+        datasets: [
+            {
+                label: "Present",
+                fillColor: "rgba(21,186,103,0.5)",
+                strokeColor: "rgba(220,220,220,0.8)",
+                highlightFill: "rgba(220,220,220,0.75)",
+                highlightStroke: "rgba(220,220,220,1)",
+                data: [ 24, 20, 22, 23, 23, 21, 20]
+            },
+            {
+                label: "Absent",
+                fillColor: "rgba(21,113,186,0.5)",
+                strokeColor: "rgba(151,187,205,0.8)",
+                highlightFill: "rgba(151,187,205,0.75)",
+                highlightStroke: "rgba(151,187,205,1)",
+                data: [ 0, 4, 2, 1, 1, 3, 4]
+            }
+        ]
+    };
 
-            var ctx2 = $("#attendance")[0].getContext("2d");
-            window.myPie = new Chart(ctx2).Pie(attendance, {
-                responsive : true,
-                showTooltips: true
-            });
+    window.onload = function(){
+        // var ctx = $(".doughnut-chart")[0].getContext("2d");
+        // window.myDoughnut = new Chart(ctx).Doughnut(doughnutData, {
+        //     responsive : true,
+        //     showTooltips: true
+        // });
 
-            // var ctx3 = $(".line-chart")[0].getContext("2d");
-            // window.myLine = new Chart(ctx3).Line(lineChartData, {
-            //     responsive : true,
-            //     showTooltips: true
-            // });
+        // var ctx2 = $("#attendance")[0].getContext("2d");
+        // window.myPie = new Chart(ctx2).Pie(attendance, {
+        //     responsive : true,
+        //     showTooltips: true
+        // });
 
-            // var ctx4 = $(".bar-chart")[0].getContext("2d");
-            // window.myBar = new Chart(ctx4).Bar(barData, {
-            //     responsive : true,
-            //     showTooltips: true
-            // });
+        // var ctx3 = $(".line-chart")[0].getContext("2d");
+        // window.myLine = new Chart(ctx3).Line(lineChartData, {
+        //     responsive : true,
+        //     showTooltips: true
+        // });
 
-            // var ctx5 = $(".radar-chart")[0].getContext("2d");
-            // window.myRadar = new Chart(ctx5).Radar(radarData, {
-            //     responsive : true,
-            //     showTooltips: true
-            // });
+        var ctx4 = $("#bar")[0].getContext("2d");
+        window.myBar = new Chart(ctx4).Bar(bar, {
+            responsive : true,
+            showTooltips: true
+        });
 
-            // var ctx6 = $(".polar-chart")[0].getContext("2d");
-            // window.myPolar = new Chart(ctx6).PolarArea(doughnutData, {
-            //     responsive : true,
-            //     showTooltips: true
-            // });
-        };
-    })(jQuery);
+        // var ctx5 = $(".radar-chart")[0].getContext("2d");
+        // window.myRadar = new Chart(ctx5).Radar(radarData, {
+        //     responsive : true,
+        //     showTooltips: true
+        // });
+
+        // var ctx6 = $(".polar-chart")[0].getContext("2d");
+        // window.myPolar = new Chart(ctx6).PolarArea(doughnutData, {
+        //     responsive : true,
+        //     showTooltips: true
+        // });
+    };
+})(jQuery);
