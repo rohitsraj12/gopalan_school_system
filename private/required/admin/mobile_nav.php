b355b3ae95d9202cf2ee3aa44ffe6d3e3ce6fb3b
Rohit <rohitsraj12@gmail.com>
2021-07-09 18:35:38 +0530

    Create mobile_nav.php

---
+++ private/required/admin/mobile_nav.php
@@ -0,0 +1,80 @@
+</div>
+<!-- start: Mobile -->
+<div id="mimin-mobile" class="reverse">
+        <div class="mimin-mobile-menu-list">
+            <div class="col-md-12 sub-mimin-mobile-menu-list animated fadeInLeft">
+                <ul class="nav nav-list">
+                    
+                    <!-- <li class="active ripple">
+                      <a class="tree-toggle nav-header"><span class="fa-home fa"></span> Dashboard 
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                      <ul class="nav nav-list tree">
+                          <li><a href="<?php base_url()?>admin/">index</a></li>
+                      </ul>
+                    </li> -->
+                    <li  class="active ripple"><a href="<?php base_url()?>admin/"><span class="fa-home fa"></span>Dashboard</a></li>
+                    <li class="ripple">
+                      <a class="tree-toggle nav-header">
+                        <span class="fa-diamond fa"></span> Teachers
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                      <ul class="nav nav-list tree">
+                        <li><a href="<?php base_url();?>admin/teacher/">View Teacher list</a></li>
+                        <li><a href="<?php base_url();?>admin/teacher/create_new_teacher.php">Create Teacher Profile</a></li>
+                        <!-- <li><a href="boxed.html">Boxed</a></li> -->
+                      </ul>
+                    </li>
+                    <li class="ripple">
+                      <a class="tree-toggle nav-header">
+                        <span class="fa-diamond fa"></span> Students
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                      <ul class="nav nav-list tree">
+                        <li><a href="<?php base_url();?>admin/students/">View Student list</a></li>
+                        <li><a href="<?php base_url();?>admin/students/create_new_student.php">Create Student Profile</a></li>
+                        <!-- <li><a href="boxed.html">Boxed</a></li> -->
+                      </ul>
+                    </li>
+                    <li class="ripple">
+                      <a class="tree-toggle nav-header">
+                        <span class="fa-area-chart fa"></span>Notice Board
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                      <ul class="nav nav-list tree">
+                        <li><a href="<?php base_url();?>admin/notice_board/index.php">View</a></li>
+                        <li><a href="<?php base_url();?>admin/notice_board/create_notice.php">Create</a></li>
+                      </ul>
+                    </li>
+                    <li class="ripple">
+                      <a class="tree-toggle nav-header">
+                        <span class="fa-area-chart fa"></span>Leads
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                      <ul class="nav nav-list tree">
+                        <li><a href="<?php base_url();?>admin/leads/index.php">All</a></li>
+                        <li><a href="chartjs.html">Create</a></li>
+                      </ul>
+                    </li>
+                    <li class="ripple">
+                      <a class="tree-toggle nav-header">
+                        <span class="fa-area-chart fa"></span>Testimonial
+                        <span class="fa-angle-right fa right-arrow text-right"></span>
+                      </a>
+                      <ul class="nav nav-list tree">
+                        <li><a href="<?php base_url();?>index.php">View</a></li>
+                        <li><a href="chartjs.html">Create</a></li>
+                      </ul>
+                    </li>
+                    
+                    <li><a href="<?php base_url();?>logout.php">Logout</a></li>
+                    
+                    <!-- <li><a href="credits.html">Credits</a></li> -->
+                  </ul>
+            </div>
+        </div>       
+      </div>
+      <button id="mimin-mobile-menu-opener" class="animated rubberBand btn btn-circle btn-danger">
+        <span class="fa fa-bars"></span>
+      </button>
+       <!-- end: Mobile -->
