bfc71198b1bce929fb2d50bdb892b71adfce532d
Rohit <rohitsraj12@gmail.com>
2021-07-14 12:54:52 +0530

    Update take_attendance.php

--- public/dashboard/attendance/take_attendance.php
+++ public/dashboard/attendance/take_attendance.php
@@ -1,103 +1,119 @@
 <?php
- // add new teacher in teacher record
+ // Take Attendance page
     session_start();
     if(!isset($_SESSION["emp_id"])){
         header('location: ../../teacher_login.php');
     }
-
+    $id = $_SESSION['emp_id'];
     $page_title = "Take Attendance";
 
     include('../../../private/config/db_connection/db_connect.php');
-    // include('include/school_name.php');
     include('../../../private/config/config.php');
-
-    $id = $_SESSION['emp_id'];
 
     $query = "SELECT * FROM teachers WHERE emp_id='$id'";
     $result = mysqli_query($conn, $query);
-
     $row = mysqli_fetch_assoc($result);
-    echo $row['first_name'];
 
     include('../../../private/required/teacher/teacher_header.php');
+
+    // capture from url
+    $std = $_GET['class'];
+    $div = $_GET['section'];
 ?>
 
     <!-- start: Content -->
     <div id="content">
         <div class="panel box-shadow-none content-header">
             <div class="panel-body">
-            <div class="col-md-12">
-                <h3 class="animated fadeInLeft">Class Room</h3>
-                <p class="animated fadeInDown">
-                    Class Room <span class="fa-angle-right fa"></span> Attendance
-                </p>
-            </div>
+                <div class="col-md-12">
+                    <h3 class="animated fadeInLeft">Take Attendance</h3>
+                    <p class="animated fadeInDown">
+                        Class Room <span class="fa-angle-right fa"></span> Attendance
+                    </p>
+                </div>
             </div>
         </div>
 
         <div class="col-md-12 top-20 padding-0">
-        <div class="col-md-12">
-            <div class="panel">
+            <div class="col-md-12">
+                <div class="panel">
 
-                    
+                <div class="panel-heading"><h3>Take Attendance of <?php echo $std;?>th Standard</h3></div>
+                    <div class="panel-body">
+                        <form action="include/attendance.inc.php" method="POST">
 
+                            <input type="hidden" name="teacher_id" value="<?php echo $row['emp_id'];?>">
+                            <input type="hidden" name="std" value="<?php echo $std;?>">
+                            <input type="hidden" name="div" value="<?php echo $div;?>">
 
-            <div class="panel-heading"><h3>Class Rooms</h3></div>
-            <div class="panel-body">
-            <form action="">
-                <div class="responsive-table">
-                
-                <table id="datatables-example" class="table table-striped table-bordered" width="100%" cellspacing="0">
-                    <thead>
-                    <tr>
-                        <th>S.N</th>
-                        <th>Student Id</th>
-                        <th>Student Name</th>
-                        <th>Take Attendance</th>
-                        <th>Student Note</th>
-                        <!-- <th>Notice Board</th>
-                        <th>Test Anouncement</th>
-                        <th>Homework</th> -->
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                    $student_query = "SELECT * FROM students";
-                    $student_result = mysqli_query($conn, $student_query);
+                            <div class="responsive-table">
+                            
+                                <table id="datatables-example" class="table table-striped table-bordered" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>S.N</th>
+                                            <th>Student Id</th>
+                                            <th>Student Name</th>
+                                            <th>Take Attendance</th>
+                                            <th>Status</th>
+                                            <th>Student Note</th>
+                                           <!--  <th>Test Anouncement</th>
+                                            <th>Homework</th> -->
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
 
-                    while($rows = mysqli_fetch_assoc($student_result)){
-                    ?>
-                        <tr>
-                        <td><?php echo $rows['student_id'];?></td>
-                        <td><?php echo $rows['student_user_id'];?></td>
+                                            $student_query = "SELECT * FROM students WHERE class_id='$std' AND section_id = '$div'";
+                                            $student_result = mysqli_query($conn, $student_query);
 
-                        <td><?php echo $rows['first_name'] . " " . $rows['last_name'];?></td>
+                                            while($rows = mysqli_fetch_assoc($student_result)){
+                                        ?>
+                                        <tr>
+                                            <td>
+                                                <?php echo $rows['student_id'];?>
+                                                <input type="hidden" name="student_id[]" value="<?php echo $rows['student_id'];?>">
+                                            </td>
+                                            <td>
+                                                <?php echo $rows['student_user_id'];?>
+                                                <input type="hidden" name="student_user_id[]" value="<?php echo $rows['student_user_id'];?>">
+                                                
+                                            </td>
 
-                        <td>
-                            <div class="col-md-6">
-                                <button class="btn btn-success">Present</button>
+                                            <td><?php echo $rows['first_name'] . " " . $rows['last_name'];?></td>
+
+                                            <td>
+                                                <div class="col-md-6">
+                                                    <a href="#" class="btn btn-success attend" data-attendance="present">Present</a>
+                                                </div>
+
+                                                <div class="col-md-6">
+                                                    <a href="#" class="btn btn-danger attend" data-attendance="absent">Absent</a>
+                                                </div>
+
+                                                <input type="hidden" name="attendance[]" value="present" checked>
+                                            </td>
+                                            <td class="status">
+                                                Present
+                                            </td>
+                                            <td><?php echo "Student can leave message"; ?></td>
+                                            <!-- <td><a href="view_student_profile.php?id=<?php echo $rows['student_user_id'];?>">view details</a></td> -->
+                                        </tr>
+
+                                        <?php
+                                            }
+                                        ?>
+                                    </tbody>
+                                </table>
+
+                                
+
                             </div>
-                            <div class="col-md-6">
-                                <button class=" btn btn-danger">Absent</button>
-                            </div>
-
-                        </td>
-                        <td><?php echo "Student can leave message"; ?></td>
-                        <!-- <td><a href="update_students_profile.php?id=<?php echo $rows['student_user_id'];?>">update profile</a></td>
-                        <td><a href="view_student_profile.php?id=<?php echo $rows['student_user_id'];?>">view details</a></td> -->
-                        </tr>
-
-                        <?php
-                        }
-                    ?>
-                    </tbody>
-                </table>
+                            <input type="submit" value="save" name="submit">
+                        </form>
+                    </div>
                 </div>
-
-            </form>
-            </div>
-        </div>
-        </div>  
+            </div>  
         </div>
     </div>
     <!-- end: content -->
