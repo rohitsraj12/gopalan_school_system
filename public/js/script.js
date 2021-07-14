a2f7490e5217954ed358ee6ba05351fb4e02f9e7
Rohit <rohitsraj12@gmail.com>
2021-07-14 12:55:07 +0530

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,30 @@
 $(document).ready(function() {
     $('.other-table').DataTable();
-} );
+
+    // attendance 
+
+    $(".attend").click(function(e){
+        e.preventDefault();
+        var btn = $(this).attr("data-attendance");
+        // console.log(btn);
+        $(this).parents('td').find('input').attr("value", btn);
+
+        var inputVal = $(this).parents('td').find('input').val();
+        
+        // alert (inputVal);
+        if(inputVal == "absent"){
+            // $(this).toggleClass('btn-danger');
+            $(this).parents('tr').removeClass('alert-info');
+            $(this).parents('tr').addClass('text-danger');
+            $(this).parents('tr').find(".status").addClass('alert-danger');
+            $(this).parents('tr').find(".status").text('Absent');
+            // alert('red');
+        }else if(inputVal == "present"){
+            $(this).parents('tr').removeClass('text-danger');
+            $(this).parents('tr').addClass('text-dark');
+            $(this).parents('tr').find(".status").removeClass('alert-danger');
+            $(this).parents('tr').find(".status").text('Present');
+        }
+    });
+
+});
