6c5ab6822fad2080367724d1b35598092399eb45
Rohit <rohitsraj12@gmail.com>
2021-09-09 17:48:50 +0530

    Update lead.php

--- public/api/get/lead.php
+++ public/api/get/lead.php
@@ -1,5 +1,9 @@
 <?php
 
+
+header('Content-Type: application/json');
+header('Acess-Control-Allow-Origin: *');
+
 // fetch teachers on api
 
 include('../../../private/config/db_connection/db_connect.php');
@@ -7,6 +11,12 @@ include('../../../private/config/db_connection/db_connect.php');
 $query    = "SELECT * FROM admins";
 $result   = mysqli_query($conn, $query);
 
-while($rows = mysqli_fetch_assoc($result)){
-    echo $rows['admin_id'];
+
+if(mysqli_num_rows($result) > 0){
+    
+    $output = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    echo json_encode($output);
+
+} else {
+    echo json_encode(array('message' => 'No record found', 'status' => false));
 }
